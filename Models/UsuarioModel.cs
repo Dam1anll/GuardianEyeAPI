f538d2258f4717615784ab2c8fbf8fa6b71570ea
Dam1anll <damiianll20@gmail.com>
2024-02-29 13:22:47 -0700

    .

--- Models/UsuarioModel.cs
+++ Models/UsuarioModel.cs
@@ -7,7 +7,7 @@ namespace GuardianEyeAPI.Models
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; } = string.Empty;
+        public string Id = string.Empty;
 
         [BsonElement("Nombre")]
         public string Nombre { get; set; } = string.Empty;
